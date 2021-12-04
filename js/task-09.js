b760b0f7a0652a0ac2755c834f53bd4798228316
Mashtaler Olha <mashtaler887@gmail.com>
2021-12-05 00:18:40 +0200

    added trimStart for task5, added default color for task9

--- js/task-09.js
+++ js/task-09.js
@@ -2,11 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const spanColorEl = document.querySelector('.color')
-const btnEl = document.querySelector('.change-color')
+const spanColorEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+spanColorEl.textContent = window.getComputedStyle(document.body)['backgroundColor'];
 
 btnEl.addEventListener('click', () => {
-
   spanColorEl.textContent
    = document.body.style.backgroundColor
    = getRandomHexColor();
